53d1ecc0558f9ec7d312e9d6120b6a7c9cf69358
Kortegaard <kortegaard@math.au.dk>
2022-05-26 12:17:44 +0200

    bug fixing

--- src/Quivers.jl
+++ src/Quivers.jl
@@ -6,15 +6,15 @@ include("path.jl")
 
 export Quiver, Vertex, Arrow, Path
 
-export add_arrow!, add_vertex!, add_vertices!,
-export find_vertex, find_arrow!, 
+export add_arrow!, add_vertex!, add_vertices!
+export find_vertex, find_arrow!
 export vertices, num_vertices
 export arrows, exists_arrow, find_arrow, num_arrows
 export neighbours
 
 include("plot.jl")
 ## Plot
-export normalize_quiver!,
+export normalize_quiver!
 export set_random_position!, spring_step
 
 
